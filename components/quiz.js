f44723ee589a9f12199c3e7a55560f3c63c64c9a
Victor Grau Serrat <lacabra@users.noreply.github.com>
2020-09-05 23:43:06 -0600

    BLD: refactored quiz from class to fn

--- components/quiz.js
+++ components/quiz.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import dynamic from 'next/dynamic'
 import {Row, Col, Button} from 'react-bootstrap';
@@ -10,84 +10,84 @@ const MapComponent = dynamic(import('../components/mapComponent'),{
 	ssr: false
 })
 
-class Quiz extends Component {
+export default function Quiz (props) {
 
-	constructor(props) {
-		super(props);
-		this.state = {next: false, yes: 'outline-primary', no: 'outline-primary', result: false, answerClass: 'answerHidden', answer:''};
+	const [next, setNext] = useState(false);
+	const [yes, setYes] = useState('outline-primary');
+	const [no, setNo] = useState('outline-primary');
+	const [result, setResult] = useState(false);
+	const [answer, setAnswer] = useState({answer:'', answerClass: 'answerHidden'});
 
-		// This binding is necessary to make `this` work in the callback
-		this.handleClick = this.handleClick.bind(this);
-		this.handleNext = this.handleNext.bind(this);
-  	}
-
-  	handleClick(e, value) {
-  		let buttonVariant = '';
-  		if(value === this.props.answer) {
-  			if(value === true) {
-  				this.setState(state => ({yes: 'success', answerClass: 'answerCorrect', answer: 'Correct answer'}));
-  			} else {
-  				this.setState(state => ({no: 'success', answerClass: 'answerCorrect', answer: 'Correct answer'}));
-  			}
-  			this.setState(state => ({result: true}))
-  		} else {
-  			if(value === true) {
-  				this.setState(state => ({yes: 'danger', answerClass: 'answerIncorrect', answer: 'Incorrect answer'}));
-  			} else {
-  				this.setState(state => ({no: 'danger', answerClass: 'answerIncorrect', answer: 'Incorrect answer'}));
-  			}
-  			this.setState(state => ({result: false}))
-  		}
-  		this.setState(state => ({next: true}));
-  	}
-
-  	handleNext(){
-		this.setState(state => ({next: false, yes: 'outline-primary', no: 'outline-primary', answerClass: 'answerHidden', answer: ''}));
-		this.props.onAnswerSelected(this.state.result)
-  	}
-
-	render() {
-
-		const latlon = [this.props.lat, this.props.lon]
-		const answerClass = "answer " + this.state.answerClass
-		return (
-			<Layout myClass="quiz">
-				<Head>
-					<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				</Head>
-				<main>
-					<QuestionCount counter={this.props.questionId} total={this.props.questionTotal} />
-					<p>Does this look like a school location?</p>
-
-					<div className="row no-gutters align-items-center mapdiv">
-						<MapComponent lat={this.props.lat} lon={this.props.lon} />
-						<div className={answerClass}>
-							{this.state.answer}
-						</div>
-					</div>
-
-					<div>
-						<Row className="p-3">
-							<Col xs={4}>
-								<Button variant={this.state.yes} onClick={e => this.handleClick(e, true)} disabled={this.state.next}>Yes</Button>{' '}
-							</Col>
-							<Col xs={4}>
-								<Button variant={this.state.no} onClick={e => this.handleClick(e, false)} disabled={this.state.next}>No</Button>{' '}
-							</Col>
-							<Col xs={4}>
-								<Button variant="outline-secondary" disabled>Maybe</Button>{' '}
-							</Col>
-						</Row>
-					</div>
-				</main>
-
-				<footer className="mt-auto">
-					<Button variant="primary" onClick={this.handleNext} disabled={!this.state.next}><span>NEXT<img className="white" src="/white.svg"/></span></Button>{' '}
-				</footer>
-
-			</Layout>
-		)
+	function handleClick(e, value) {
+		let buttonVariant = '';
+		if(value === props.answer) {
+			if(value === true) {
+				setYes('success');
+				setAnswer({answer: 'Correct answer', answerClass: 'answerCorrect'});
+			} else {
+				setNo('success');
+				setAnswer({answer: 'Correct answer', answerClass: 'answerCorrect'});
+			}
+			setResult(true);
+		} else {
+			if(value === true) {
+				setYes('danger');
+				setAnswer({answer: 'Incorrect answer', answerClass: 'answerIncorrect'});
+			} else {
+				setNo('danger');
+				setAnswer({answer: 'Incorrect answer', answerClass: 'answerIncorrect'});
+			}
+			setResult(false);
+		}
+		setNext(true);
 	}
-}
 
-export default Quiz;
+	function handleNext(){
+		setNext(false);
+		setYes('outline-primary');
+		setNo('outline-primary');
+		setAnswer({answer: '', answerClass: 'answerHidden'});
+		props.onAnswerSelected(result);
+  	}
+
+	const latlon = [props.lat, props.lon];
+	const answerClass = "answer " + answer.answerClass
+
+	return (
+		<Layout myClass="quiz">
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			</Head>
+			<main>
+				<QuestionCount counter={props.questionId} total={props.questionTotal} />
+				<p>Does this look like a school location?</p>
+
+				<div className="row no-gutters align-items-center mapdiv">
+					<MapComponent lat={props.lat} lon={props.lon} />
+					<div className={answerClass}>
+						{answer['answer']}
+					</div>
+				</div>
+
+				<div>
+					<Row className="p-3">
+						<Col xs={4}>
+							<Button variant={yes} onClick={e => handleClick(e, true)} disabled={next}>Yes</Button>{' '}
+						</Col>
+						<Col xs={4}>
+							<Button variant={no} onClick={e => handleClick(e, false)} disabled={next}>No</Button>{' '}
+						</Col>
+						<Col xs={4}>
+							<Button variant="outline-secondary" disabled>Maybe</Button>{' '}
+						</Col>
+					</Row>
+				</div>
+			</main>
+
+			<footer className="mt-auto">
+				<Button variant="primary" onClick={handleNext} disabled={!next}><span>NEXT<img className="white" src="/white.svg"/></span></Button>{' '}
+			</footer>
+
+		</Layout>
+	)
+}
