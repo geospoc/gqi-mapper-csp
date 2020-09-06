e1487f0da31c7985d77ecb4f0c987b0e6aac8765
Victor Grau Serrat <lacabra@users.noreply.github.com>
2020-09-05 23:17:17 -0600

    BLD: refactored mapComponent from class to fn

--- components/mapComponent.js
+++ components/mapComponent.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactMapboxGl, { Layer, Feature, Marker, ZoomControl } from 'react-mapbox-gl';
 
 const zoomDefault = 17;
@@ -9,56 +9,37 @@ const Map = ReactMapboxGl({
   minZoom: 15
 });
 
-class mapComponent extends Component {
+export default function mapComponent(props) {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			lon: props.lon,
-			lat: props.lat,
-			zoom: zoomDefault,
-			lonMarker: props.lon,
-			latMarker: props.lat
-		};
-	}
+	const [zoom, setZoom] = useState(zoomDefault);
+	const [lonLat, setLonLat] = useState([props.lon, props.lat]);
+	const [lonLatMarker, setLonLatMarker] = useState([props.lon, props.lat]);
 
-	componentDidUpdate(prevProps) {
-		if(this.props.lat !== prevProps.lat) {
-			this.setState({
-				lat: this.props.lat,
-				lon: this.props.lon,
-				zoom: zoomDefault,
-				lonMarker: this.props.lon,
-				latMarker: this.props.lat
-			});
-		}
-	}
+	useEffect(() => {
+		setZoom(zoomDefault);
+		setLonLat([props.lon, props.lat]);
+		setLonLatMarker([props.lon, props.lat]);
+	}, [props.lon, props.lat]);
 
-	render() {
-
-		return (
-			<Map
-				style="mapbox://styles/mapbox/satellite-v9"
-				center={[this.state.lon, this.state.lat]}
-				zoom={[this.state.zoom]}
-				containerStyle={{ width: '100%', height: '100%' }}
-				movingMethod='jumpTo'
-				onMoveEnd={(map) => this.setState(state => ({
-						lon: map.getCenter().lng,
-							lat: map.getCenter().lat,
-						zoom: map.getZoom()
-					})
-				)}
-			>
-				<Marker
-					coordinates={[this.state.lonMarker, this.state.latMarker]}
-					anchor="bottom">
-					<img src='/marker.svg'/>
-				</Marker>
-				<ZoomControl/>
-			</Map>
-		);
-	}
+	return (
+		<Map
+			style="mapbox://styles/mapbox/satellite-v9"
+			center={lonLat}
+			zoom={[zoom]}
+			containerStyle={{ width: '100%', height: '100%' }}
+			movingMethod='jumpTo'
+			onMoveEnd={(map) => {
+					setZoom(map.getZoom());
+					setLonLat([map.getCenter().lng, map.getCenter().lat]);
+				}
+			}
+		>
+			<Marker
+				coordinates={lonLatMarker}
+				anchor="bottom">
+				<img src='/marker.svg'/>
+			</Marker>
+			<ZoomControl/>
+		</Map>
+	);
 }
-
-export default mapComponent;
