af9b8c5c3e947a8307f61bfb526cbf890dff5ae8
CallaJun <calla.carter@yahoo.com>
2020-11-19 22:15:01 -0500

    Filtered out locations already tagged by users
    * Reordered events, placing the creation of a uuid when one does not exist before the API call to get locations.
    * Changed the API call to accept a uuid.
    * Filtered out locations already tagged by user by editing SQL query.

---
+++ pages/api/getLocations/[uuid].js
@@ -0,0 +1,42 @@
+import { validate as uuidValidate } from 'uuid';
+
+const { Pool } = require('pg');
+
+const pool = new Pool()
+
+export default async (req, res) => {
+	if (req.method === 'GET') {
+		let result = null;
+		const user_id = req.query.uuid;
+		if (user_id && uuidValidate(user_id)) {
+			try {
+				result = await pool.query(`SELECT locations.school_id,
+											       locations.lat,
+											       locations.lon
+											FROM locations
+											LEFT JOIN
+											  (SELECT school_id
+											   FROM crowdsourcing
+											   WHERE user_id != '${user_id}') AS seen ON locations.school_id = seen.school_id
+											WHERE seen.school_id IS NULL
+											ORDER BY random() LIMIT 10;`);
+			} catch(e) {
+				console.log(e)
+			}	
+		}
+
+		let output = null;
+		if(result) {
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'application/json');
+			output = JSON.stringify(result.rows)
+		} else {
+			res.statusCode = 500
+		}
+		res.end(output);
+	} else {
+		// If it's not a GET request, return 405 - Method Not Allowed
+		res.statusCode = 405;
+		res.end();
+	}
+}
