9c8f754bf797be57322e96e46108f690780b6003
EkkuJ <ekku.jokinen@aalto.fi>
2021-02-03 19:43:35 +0200

    BLD: Add API endpoint to add uuid-value to user

---
+++ pages/api/saveUserData.js
@@ -0,0 +1,33 @@
+import { validate as uuidValidate } from 'uuid'
+
+const { Pool } = require('pg')
+const pool = new Pool()
+
+export default async (req, res) => {
+    // Allow POST-requests only
+    if (req.method !== 'POST') {
+        res.statusCode = 405
+        res.end()
+    }
+
+    const user_id = req.body.user_id
+    const uuid = req.body.uuid
+
+    if (!(user_id && uuid && uuidValidate(uuid))) {
+        res.statusCode = 400
+        res.end()
+    }
+
+    const query = 'UPDATE users SET uuid=($1) WHERE id=($2) AND uuid IS NULL RETURNING *;'
+
+    pool.query(query, [uuid, user_id])
+        .then((result) => {
+            res.statusCode = 200
+            res.end()
+        })
+        .catch((err) => {
+            console.log(err.stack)
+            res.statusCode = 404
+            res.end()
+        })
+}
