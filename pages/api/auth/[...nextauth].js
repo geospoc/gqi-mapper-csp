57bd991d2f5ae8fbcb4094f8f6364a81a4e6744f
Victor Grau Serrat <lacabra@users.noreply.github.com>
2021-04-07 13:40:48 -0600

    BLD: added GitHub Oauth provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -20,14 +20,18 @@ export default (req, res) => {
                 clientId: process.env.FACEBOOK_ID,
                 clientSecret: process.env.FACEBOOK_SECRET,
             }),
+            Providers.Twitter({
+                clientId: process.env.TWITTER_CLIENT_ID,
+                clientSecret: process.env.TWITTER_CLIENT_SECRET,
+            }),
+            Providers.GitHub({
+                clientId: process.env.GITHUB_CLIENT_ID,
+                clientSecret: process.env.GITHUB_CLIENT_SECRET
+            }),
             Providers.Google({
                 clientId: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET
             }),
-            Providers.Twitter({
-                clientId: process.env.TWITTER_CLIENT_ID,
-                clientSecret: process.env.TWITTER_CLIENT_SECRET,
-            })
         ],
 
         // https://next-auth.js.org/schemas/adapters
