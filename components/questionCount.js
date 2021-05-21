c7d0187cca6f928108489521b5d05a064e8d4923
Rachita Saha <rachita19082@iiitd.ac.in>
2021-05-21 17:43:24 +0530

    Fix linting and formatting

--- components/questionCount.js
+++ components/questionCount.js
@@ -20,113 +20,117 @@ function QuestionCount(props) {
       <span>{props.counter + 1}</span>/<span>{props.total}</span>
       {!isMobile && (
         <>
-        <button
-          type="button"
-          onClick={handleShow}
-          className="btn btn-warning float-right"
-          style={{
-            color: "white",
-            fontSize: "14px",
-            backgroundColor: "#18A0FB",
-            borderWidth: 0,
-          }}
-        >
-          Keyboard Shortcuts
-        </button>
-        <Modal size="sm" show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title style={{fontSize: "16px", fontWeight: "bold"}}>
-              Keyboard Shortcuts
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body style={{color: "gray", padding: "22px"}}>
-            <Button
-              className="yes actionButton"
-              variant="outline-primary"
-              style={{width: "120px"}}
-            >
-              Yes
-            </Button>
-            <Button
-              style={{
-                width: "50px",
-                backgroundColor: "gray",
-                border: 0,
-                marginLeft: "60px",
-              }}
-            >
-              <ArrowUpwardRoundedIcon />
-            </Button>
-            <br />
-            <br />
-            <Button
-              className="no actionButton"
-              variant="outline-primary"
-              style={{width: "120px"}}
-            >
-              No
-            </Button>
-            <Button
-              style={{
-                width: "50px",
-                backgroundColor: "gray",
-                border: 0,
-                marginLeft: "60px",
-              }}
-            >
-              <ArrowDownwardRoundedIcon />
-            </Button>
-            <br />
-            <br />
-            {displayUnsure && (
-              <>
-                <Button
-                  className="maybe actionButton"
-                  variant="outline-primary"
-                  style={{width: "120px"}}
-                >
-                  Unsure
-                </Button>
-                <Button
-                  style={{
-                    width: "50px",
-                    backgroundColor: "gray",
-                    border: 0,
-                    marginLeft: "60px",
-                  }}
-                >
-                  <ArrowBackRoundedIcon />
-                </Button>
-                <br />
-                <br />
-              </>
-            )}
-            <Button
-              className="actionButton"
-              variant="outline-primary"
-              style={{width: "120px"}}
-            >
-              Next
-            </Button>
-            <Button
-              style={{
-                width: "50px",
-                backgroundColor: "gray",
-                border: 0,
-                marginLeft: "60px",
-              }}
-            >
-              <ArrowForwardRoundedIcon />
-            </Button>
-            <br />
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="primary" onClick={handleClose} style={{margin: 0, padding: 8}}>
-              Okay
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
+          <button
+            type="button"
+            onClick={handleShow}
+            className="btn btn-warning float-right"
+            style={{
+              color: "white",
+              fontSize: "14px",
+              backgroundColor: "#18A0FB",
+              borderWidth: 0,
+            }}
+          >
+            Keyboard Shortcuts
+          </button>
+          <Modal size="sm" show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title style={{fontSize: "16px", fontWeight: "bold"}}>
+                Keyboard Shortcuts
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body style={{color: "gray", padding: "22px"}}>
+              <Button
+                className="yes actionButton"
+                variant="outline-primary"
+                style={{width: "120px"}}
+              >
+                Yes
+              </Button>
+              <Button
+                style={{
+                  width: "50px",
+                  backgroundColor: "gray",
+                  border: 0,
+                  marginLeft: "60px",
+                }}
+              >
+                <ArrowUpwardRoundedIcon />
+              </Button>
+              <br />
+              <br />
+              <Button
+                className="no actionButton"
+                variant="outline-primary"
+                style={{width: "120px"}}
+              >
+                No
+              </Button>
+              <Button
+                style={{
+                  width: "50px",
+                  backgroundColor: "gray",
+                  border: 0,
+                  marginLeft: "60px",
+                }}
+              >
+                <ArrowDownwardRoundedIcon />
+              </Button>
+              <br />
+              <br />
+              {displayUnsure && (
+                <>
+                  <Button
+                    className="maybe actionButton"
+                    variant="outline-primary"
+                    style={{width: "120px"}}
+                  >
+                    Unsure
+                  </Button>
+                  <Button
+                    style={{
+                      width: "50px",
+                      backgroundColor: "gray",
+                      border: 0,
+                      marginLeft: "60px",
+                    }}
+                  >
+                    <ArrowBackRoundedIcon />
+                  </Button>
+                  <br />
+                  <br />
+                </>
+              )}
+              <Button
+                className="actionButton"
+                variant="outline-primary"
+                style={{width: "120px"}}
+              >
+                Next
+              </Button>
+              <Button
+                style={{
+                  width: "50px",
+                  backgroundColor: "gray",
+                  border: 0,
+                  marginLeft: "60px",
+                }}
+              >
+                <ArrowForwardRoundedIcon />
+              </Button>
+              <br />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button
+                variant="primary"
+                onClick={handleClose}
+                style={{margin: 0, padding: 8}}
+              >
+                Okay
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </>
       )}
     </h2>
   );
