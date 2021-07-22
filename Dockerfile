4a729fac1efa6482ed7a3c373a815480cf8c0a0a
Shreya Galani <shreya.galani@geospoc.com>
2021-07-23 00:57:32 +0530

    chore: temp fix

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,12 @@
-FROM node:15
+FROM node:14
 
-WORKDIR app/
+WORKDIR /app/
 
 COPY ["package.json", "package-lock.json*", "./"]
 
-RUN npm install -g npm@7.19.1
 RUN npm ci --no-audit
 
 COPY . .
 RUN npm run build
 
-EXPOSE 8000
-ENV PORT=8000
-CMD [ "npm", "run", "start" ]
+CMD ["npm", "run", "start", "--", "-p", "8000"]
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,7 +10,7 @@ const DATABASE_URL = `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}
 
 export default (req, res) => {
   const cookies = new Cookies(req, res);
-
+  console.log(Adapters);
   // For more information on each option (and a full list of options) go to
   // https://next-auth.js.org/configuration/options
   return NextAuth(req, res, {
@@ -155,6 +155,6 @@ export default (req, res) => {
     events: {},
 
     // Enable debug messages in the console if you are having problems
-    debug: false,
+    debug: true,
   });
 };
